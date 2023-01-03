8eae2d2697057dfbfdb5f8b880332c83a433bae2
Dini68 <kdini68@gmail.com>
2023-01-03 12:34:16 +0100

    Autómárka feladat kész.

---
+++ src/main/java/linebreak/Car.java
@@ -0,0 +1,16 @@
+package linebreak;
+
+public class Car {
+
+    public String getBrandAndTypeInSeparateLines(String brand,
+                                                 String type) {
+        return brand + "\n" + type;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(
+                car.getBrandAndTypeInSeparateLines(
+                        "Hyundai","ix35"));
+    }
+}
