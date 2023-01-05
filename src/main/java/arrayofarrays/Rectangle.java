14a60ceaa1d02982ccb98bb7c4adcb498833ca28
Dini68 <kdini68@gmail.com>
2023-01-05 12:19:47 +0100

    Háromszögmátrix feladat kész.

---
+++ src/main/java/arrayofarrays/Rectangle.java
@@ -0,0 +1,31 @@
+package arrayofarrays;
+
+public class Rectangle {
+
+    public int[][] rectangularMatrix(int size) {
+
+        int[][] rectangular = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                rectangular[i][j] = i;
+            }
+        }
+
+        return rectangular;
+    }
+
+    public static void main(String[] args) {
+
+        Rectangle re = new Rectangle();
+
+        int[][] result = re.rectangularMatrix(4);
+
+        for (int[] i : result) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
