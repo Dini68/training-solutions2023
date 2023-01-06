aeda2c63aea6490c02b14543c3335d3d3a85cd0b
Dini68 <kdini68@gmail.com>
2023-01-06 14:00:52 +0100

    A Math osztály kipróbálása feladat kész.

---
+++ src/main/java/math/math/MathMethods.java
@@ -0,0 +1,30 @@
+package math.math;
+
+public class MathMethods {
+
+    public static void main(String[] args) {
+
+        System.out.println("Math.max(45, 75)= " + Math.max(45, 75));
+        System.out.println("Math.min(45, 75)= " + Math.min(45, 75));
+
+        System.out.println("Math.PI= " + Math.PI);
+        System.out.println("Math.round(Math.PI)= " + Math.round(Math.PI));
+
+        System.out.println("Math.round(1968.633)= " + Math.round(1968.6330));
+
+        System.out.println("Math.abs(-2011)= " + Math.abs(-2011));
+        System.out.println("Math.abs(2011)= " + Math.abs(2011));
+
+        System.out.println("Math.negateExact(1)= " + Math.negateExact(1));
+
+        System.out.println("Math.addExact(4, 7)= " + Math.addExact(4, 7));
+        System.out.println("Math.subtractExact(4, 7)= " + Math.subtractExact(4, 7));
+        System.out.println("Math.multiplyExact(4, 7)= " + Math.multiplyExact(4, 7));
+        System.out.println("Math.pow(4, 7)= " + Math.pow(4, 7));
+
+        System.out.println("Math.incrementExact(4)= " + Math.incrementExact(4));
+        System.out.println("Math.decrementExact(4)= " + Math.decrementExact(4));
+
+        System.out.println("Math.random()= " + Math.random());
+    }
+}
