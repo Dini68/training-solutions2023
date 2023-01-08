fd1c6ca24309c5d4b1af5cf240245e8ee161ef0e
Dini68 <kdini68@gmail.com>
2023-01-08 11:27:22 +0100

    Languages feladat kész.

---
+++ src/main/java/weektwo/Languages.java
@@ -0,0 +1,22 @@
+package weektwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+        List<String> languages = new ArrayList<>();
+
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+
+        System.out.println("Length is more than 5: ");
+        for (String s: languages) {
+            if (s.length() > 5) {
+                System.out.println(s);
+            }
+        }
+    }
+}
