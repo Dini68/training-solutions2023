0db4b194beb89cb0d13e10dd1e2ee98610ed3ec2
Dini68 <kdini68@gmail.com>
2023-01-14 22:01:16 +0100

    Óceánok feladat kész.

--- src/main/java/enumtype/firstenum/FirstEnumMain.java
+++ src/main/java/enumtype/firstenum/FirstEnumMain.java
@@ -6,6 +6,13 @@ public class FirstEnumMain {
 
     public static void main(String[] args) {
 
+        for (Continent c : Continent.values()) {
+            System.out.println(c);
+        }
         System.out.println(Arrays.toString(Continent.values()));
+
+        System.out.println(Ocean.valueOf("PACIFIC_OCEAN"));
+        System.out.println(Ocean.valueOf("ATLANTIC_OCEAN"));
+        System.out.println(Ocean.valueOf("INDIAN_OCEAN"));
     }
 }
---
+++ src/main/java/enumtype/firstenum/Ocean.java
@@ -0,0 +1,7 @@
+package enumtype.firstenum;
+
+public enum Ocean {
+
+    ARCTIC_OCEAN, PACIFIC_OCEAN, ATLANTIC_OCEAN,
+    SOUTHERN_OCEAN, INDIAN_OCEAN
+}
