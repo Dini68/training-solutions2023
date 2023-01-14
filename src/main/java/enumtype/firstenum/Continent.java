e3af8b16b1467060349ec252f6c6fc2c78a90882
Dini68 <kdini68@gmail.com>
2023-01-14 21:06:10 +0100

    Kontinensek feladat kész.

---
+++ src/main/java/enumtype/firstenum/Continent.java
@@ -0,0 +1,7 @@
+package enumtype.firstenum;
+
+public enum Continent {
+
+    EUROPA, ASIA, NORTH_AMERICA, SOUTH_AMERICA, AFRICA,
+    AUSTRALIA, ANTARCTICA
+}
---
+++ src/main/java/enumtype/firstenum/FirstEnumMain.java
@@ -0,0 +1,11 @@
+package enumtype.firstenum;
+
+import java.util.Arrays;
+
+public class FirstEnumMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(Continent.values()));
+    }
+}
